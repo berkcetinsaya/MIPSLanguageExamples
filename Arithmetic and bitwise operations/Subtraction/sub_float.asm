3c1a61ad5f32c41f5a2dd00d5f01c70b6c9be5f2
Berk Cetinsaya <berkcetinsaya@gmail.com>
2019-10-29 14:09:47 -0500

    Created Subtraction

---
+++ Arithmetic and bitwise operations/Subtraction/sub_float.asm
@@ -0,0 +1,8 @@
+.data
+	number1: .float 10.54	#float variable number1 created
+	number2: .float 20.32	#float variable number2 created
+.text
+	lwc1 $f1, number1	#number1 loaded into register $f1
+	lwc1 $f2, number2	#number2 loaded into register $f2
+	
+	sub.s $f0, $f1, $f2	#$f0 = $f1 - $f2
